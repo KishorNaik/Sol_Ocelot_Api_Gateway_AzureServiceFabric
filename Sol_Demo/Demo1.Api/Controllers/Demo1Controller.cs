d91f5d4b998d69592fcd741b4a568ab0b5bf9557
Kishor Naik <kishor.naik011.net@gmail.com>
2021-05-03 18:06:53 +0530

    Code Commit

---
+++ Sol_Demo/Demo1.Api/Controllers/Demo1Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo1.Api.Controllers
+{
+    [Route("api/demo1")]
+    [ApiController]
+    public class Demo1Controller : ControllerBase
+    {
+        [HttpGet("demo")]
+        public IActionResult GetDemo()
+        {
+            return base.Ok("Demo 1");
+        }
+    }
+}
---
+++ Sol_Demo/Demo2.Api/Controllers/Demo2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo2.Api.Controllers
+{
+    [Route("api/demo2")]
+    [ApiController]
+    public class Demo2Controller : ControllerBase
+    {
+        [HttpGet("demo")]
+        public IActionResult GetDemo()
+        {
+            return base.Ok("Demo 2");
+        }
+    }
+}
